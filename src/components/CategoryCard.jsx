0a73731d57e41b6d63cf5dc46b05db375a26c763
elizadora <elizadoradasilva2003@gmail.com>
2025-06-01 21:40:20 -0300

    style: add responsiveness to Categories page

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -1,6 +1,6 @@
 export default function CategoryCard({category, totalDecks}){
     return (
-        <div className="bg-white-smoke lg:w-1/3 w-9/10 p-4 rounded-xl shadow-md flex justify-between font-roboto-slab text-rich-black text-[15px]">
+        <div className="bg-white-smoke lg:w-1/3 w-8/10 p-4 rounded-xl shadow-md flex justify-between font-roboto-slab text-rich-black text-[15px]">
             <p className="">{category}</p>
             <p>{totalDecks}</p>
         </div>
--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -7,10 +7,10 @@ export default function Categories() {
         <>
             <NavbarLogged />
             <main className="flex justify-center flex-wrap gap-10">
-                <select className="bg-white-smoke text-oxford-blue w-xl float-end py-2 px-3 mt-5 rounded-3xl">
+                <select className="bg-white-smoke text-oxford-blue lg:w-xl w-7/10 float-end py-2 px-3 mt-5 rounded-3xl">
                         <option>Categoria 1</option>
                 </select>
-                <div className="flex flex-wrap gap-10 items-center justify-evenly flex-col lg:flex-row mb-10">
+                <div className="flex flex-wrap gap-10 items-center justify-evenly flex-col lg:flex-row mb-10 w-full">
                     <CategoryCard category="Categoria 1" totalDecks={1} /> 
                     <CategoryCard category="Categoria 1" totalDecks={2} /> 
                     <CategoryCard category="Categoria 1" totalDecks={3} /> 
